a07cd1b25b016050a649ccb1ecb28bf5de2adb8e
Jorge Domínguez Barragán <57101444+jorgedb98@users.noreply.github.com>
2022-05-16 16:15:42 +0200

    Delete prueba.sh

--- Scripts/prueba.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH -J singularity_test
-#SBATCH -o singularity_test.out
-#SBATCH -e singularity_test.err
-#SBATCH -p long
-
-singularity exec -B /projects/regicor:/projects/regicor /home/jdominguez1/imgs/RGsetChannel.sif Rscript /path/to/scriptR
